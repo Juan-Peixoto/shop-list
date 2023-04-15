fb9f7ac87676f5d3a5ba937c7ac8bf5de108dc6c
Juan Peixoto <juanpeixoto1998@gmail.com>
2023-04-15 10:30:57 -0300

    worked on the main script

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
+const addButton = document.getElementById('addButton');
+const itensList = document.getElementById('itensList');
+let arrOfListItens = [];
+
+const removeParent = event => {
+    event.target.parentElement.remove();
+}
+
+addButton.onclick = button =>
+{
+    const form = document.getElementById('form');
+    const formData = new FormData(form);
+    const formValues = [...formData.entries()];
+
+    const itemName = formValues[0][1];
+    const itemQuantity = parseInt(formValues[1][1]);
+    const itemValue = parseFloat(formValues[2][1]);
+    const sumValues = (itemQuantity*itemValue).toFixed(2);
+
+    arrOfListItens.push([itemName, sumValues]);
+    console.log(arrOfListItens);
+
+    itensList.innerHTML +=
+    `
+        <div class="listItem">
+            <span id="itemName">${formValues[0][1]}</span>
+            <span id="itemQuantity">${itemQuantity}</span>
+            <span id="itemValue">R$ ${sumValues}</span>
+
+            <button onclick="removeParent(event)" id="removeButton">X</button>
+        </div>
+    `;
+
+    button.preventDefault();
+};
+
 /* Lista de compras
-
-1-Permitir ao usuário introduzir informações
-    (Nome do item, quantidade e valor)
-2-Recuperar informações do usuário
-3-Guardar as informações
-4-Exibir na tela lista dos itens adicionados
-5-Exibir a quantidade de itens
-6-Exibir na tela soma total dos valores de cada item
+5-Salvar valores em lista
+6-Exibir na tela soma total dos valores de cada item da lista
 7-Permitir ao usuário remover itens da lista
-8-Permitir ao usuário deletar a lista
-
 */
-
-console.log('Hello World!');
