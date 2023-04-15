6ed749a04f8f637a948575261700915912d43909
Juan Peixoto <juanpeixoto1998@gmail.com>
2023-04-14 21:43:56 -0300

    Created pseudocode

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+/* Lista de compras
+
+1-Permitir ao usuário introduzir informações
+    (Nome do item, quantidade e valor)
+2-Recuperar informações do usuário
+3-Guardar as informações
+4-Exibir na tela lista dos itens adicionados
+5-Exibir a quantidade de itens
+6-Exibir na tela soma total dos valores de cada item
+7-Permitir ao usuário remover itens da lista
+8-Permitir ao usuário deletar a lista
+
+*/
+
 console.log('Hello World!');
